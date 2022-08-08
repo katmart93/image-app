e8872d974cc3142ee901dfd02f1b8f9e96232595
katmart93 <katwlodarczyk93@gmail.com>
2022-08-08 19:20:53 +0200

    hero component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useState, useEffect } from "react";
+
+// components
+import Hero from "./components/Hero";
+
+// styles
 import "./App.css";
 
 function App() {
+  const API_KEY = "29139017-65f3e73c8123a7a3bd76688ac";
+  const [imager, setImages] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [search, setSearch] = useState("");
+  const [inputValue, setInputValue] = useState("");
+
+  // useEffect(() => {
+  //   fetch(
+  //     `https://pixabay.com/api/?key=${API_KEY}&q=${search}&image_type=photo&per_page=9&page=${currentPage}&pretty=true`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => setImages(data.hits));
+  // }, []);
+
   return (
     <div className="App">
-      <h1>hello</h1>
+      <Hero />
     </div>
   );
 }
