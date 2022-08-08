270c5b2fc58dc0d8596c002313279c3918aaf495
katmart93 <katwlodarczyk93@gmail.com>
2022-08-08 20:08:55 +0200

    cards displayed on the page

--- src/App.js
+++ src/App.js
@@ -19,13 +19,13 @@ function App() {
     setInputVal("");
   };
 
-  // useEffect(() => {
-  //   fetch(
-  //     `https://pixabay.com/api/?key=${API_KEY}&q=${search}&image_type=photo&per_page=9&page=${currentPage}&pretty=true`
-  //   )
-  //     .then((res) => res.json())
-  //     .then((data) => setImages(data.hits));
-  // }, []);
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${API_KEY}&q=${search}&image_type=photo&per_page=9&page=${currentPage}&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => setImages(data.hits));
+  }, []);
 
   return (
     <div className="App">
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,41 @@
 import React from "react";
 
 export default function Card({ images }) {
-  return <div>Card</div>;
+  return (
+    <>
+      {images.map((img) => (
+        <div key={img.id} className="card">
+          <a href={img.largeImageURL} target="_blank" rel="noopener noreferer">
+            <img src={img.webformatURL} alt="image" className="cardImage" />
+            <div className="cardInfo">
+              <p className="user">Photo by {img.user}</p>
+              <p>
+                <i className="fas fa-eye"></i>
+                <span>Views: {img.views}</span>
+              </p>
+              <p>
+                <i className="fas fa-download"></i>
+                <span>Downloads: {img.downloads}</span>
+              </p>
+              <p>
+                <i className="fas fa-heart"></i>
+                <span>Likes: {img.likes}</span>
+              </p>
+              <p>
+                <i className="fas fa-comments"></i>
+                <span>Comments: {img.comments}</span>
+              </p>
+              <div className="tags">
+                {img.tags
+                  .split(",")
+                  .map((tag, idx) =>
+                    tag.length >= 11 ? "" : <span key={idx}>#{tag}</span>
+                  )}
+              </div>
+            </div>
+          </a>
+        </div>
+      ))}
+    </>
+  );
 }
